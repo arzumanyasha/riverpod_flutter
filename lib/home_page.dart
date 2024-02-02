19de7691d3bff66c4f6c8c58f13e9ff9f5d31101
arzumanyasha <arzumanianartur0@gmail.com>
2024-02-02 16:30:40 +0300

    add listen and select methods

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,15 +6,61 @@ class MyHomePage extends ConsumerWidget {
   const MyHomePage({super.key});
 
   void onSubmit(WidgetRef ref, String value) {
-    // через notifier мы всё также получаем доступ к нашему состоянию 
-    // и вызываем метод updateName
-    // вся бизнес-логика находится в классе user.dart
     ref.read(userProvider.notifier).updateName(value);
+  }
+
+  void onSubmitAge(WidgetRef ref, String value) {
+    ref.read(userProvider.notifier).updateAge(int.parse(value));
   }
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(userProvider);
+    // сейчас метод ref.watch() перезапускает всё дерево виджетов в методе build()
+    // всякий раз когда меняется значение этого пользователя
+    // то есть если измениться значение у свойства name, то перестрйока всех виджетов 
+    // выполниться повторнов методе build
+    // то же самое произойдёт если поменяется age
+
+    // но что если мы хотим запускать перестроение дерева виджетов только при изменении
+    // определённого свойства класса User? допустим изменение name
+    //* final user = ref.watch(userProvider); *//
+
+    // то тут нам поможет метод select который реализован у каждого пользовательского провайдера
+    // можно сказать что метод select используется как фильтр и служит для прослушивания только
+    // определённого свойства
+    final name = ref.watch(userProvider.select((value) => value.name));
+
+    // Как мы уже знаем для наблюдения за состоянием провайдера мы используем метод watch(),
+    // однако кроме этого есть метод listen()
+
+    // Главная разница между методом ref.watch() и ref.listen() заключается в том что метод listen
+    // не перестраивает виджет или провайдер, а вызывает определённый метод
+    // когда меняется состояние у провайдера. К примеру мы можем показать диалоговое окно с ошибкой
+    // или snackbar, которое отразит изменение состояния в провайдере
+
+    // ref.listen() принимает 2 параметра, 1 - прослушиваемый провайдер и 2 - метод listener
+    // 2-й параметр это метод, который должен вызываться при изменении значения провайдера
+    // причём метод должен принимать 2 параметра:
+    // 1) предыдущее состояние - previous
+    // 2) и новое состояние провайдера - next
+
+    // колбэк listener() выполняется при изменении значения провайдера а не при вызове build
+    // следовательно мы можем его использовать также для запуска любого асинхронного кода
+    // например для отображения оповещения или осуществить навигацию и тд
+    // метод listen не стоит использовать внутри методов initState и других методов жц
+    ref.listen(
+      // благодаря этому селекту метод listen вызывается только при изменении age
+      userProvider.select((value) => value.age),
+      (previousState, currentState) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(
+              'Our age was $previousState and the new age $currentState',
+            ),
+          ),
+        );
+      },
+    );
 
     return Scaffold(
       appBar: AppBar(
@@ -28,8 +74,11 @@ class MyHomePage extends ConsumerWidget {
             TextField(
               onSubmitted: (value) => onSubmit(ref, value),
             ),
+            TextField(
+              onSubmitted: (value) => onSubmitAge(ref, value),
+            ),
             Text(
-              user.name,
+              name,
             ),
           ],
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -8,23 +8,9 @@ import 'package:riverpod_flutter/user.dart';
 // StateProvider
 // StateNotifier & StateNotifierProvider
 
-// в качестве первого возвращаемого типа мы сначала передадим UserNotifier
-// а в качестве второго типа мы передадим состояние класса UserNotifier - то есть User
 final userProvider = StateNotifierProvider<UserNotifier, User>((ref) {
-  //* return UserNotifier(const User(name: '', age: 0)); *//
-  // Допустим мы не хотим передавать в UserNotifier какое-либо состояние 
-  // то есть оставим конструктор пустым. Продолжение модификаций в классе User 
-  // пометка 112
   return UserNotifier();
 });
-
-// у нас есть класс User. Допустим в этой модели мы хотим поменять значения у этих 2 свойств
-// да, можно конечно обновить состояние этих двух свойств с помощью StateProvider
-// но тогда наша логика будет находится в общих виджетах homa_page
-// а такого не хотелось бы да и в реальном мире свойств всегда гораздо больше 
-// Решение - улучшенная версия StateProvider - комбинация StateNotifier & StateNotifierProvider
-// они идеально подходят для управления состоянием, которое может измениться в ответ на событие 
-// или взаимодействие с пользователем
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
