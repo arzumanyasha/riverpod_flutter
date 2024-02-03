84a87f238a8e42844953bb87a658aea5c9d227b6
arzumanyasha <arzumanianartur0@gmail.com>
2024-02-03 08:40:42 +0300

    add autoDispose and family modificators

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:riverpod_flutter/main.dart';
 
-class MyHomePage extends ConsumerWidget {
+class MyHomePage extends ConsumerStatefulWidget {
   const MyHomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // поток возвращает AsyncValue
-    final streamNumber = ref.watch(streamProvider);
-    // обработка состояния в стрим провайдере происходит точно так как и в FutureProvider
-    // таким орбазом StreamProvider заменяет собой StreamBuilder, что даёт свои преимущества
-    // например гарантию правильной обработки состояний в случае загрузки или ошибок
-    // благодаря возвращаемому AsyncValue.
-    return streamNumber.when(
+  ConsumerState<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends ConsumerState<MyHomePage> {
+  String userId = '1';
+
+  @override
+  Widget build(BuildContext context) {
+    // теперь наш провайдер поход на метод в который мы можем передать userID
+    final user = ref.watch(userFutureProvider(userId));
+    return user.when(
       data: (data) {
         return Scaffold(
           appBar: AppBar(
@@ -24,6 +27,13 @@ class MyHomePage extends ConsumerWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
+                TextField(
+                  onSubmitted: (value) {
+                    userId = value;
+                    // ТОЛЬКО ДЛЯ ПРИМЕРА
+                    setState(() {});
+                  },
+                ),
                 Text(
                   data.toString(),
                 ),
